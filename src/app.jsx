4b2f2ef186f2a613a684f62ab30bef7d62dab379
Victor-t814 <victor.trail001@student.swic.edu>
2024-05-15 18:58:44 -0500

    Props to First Compoenent

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,12 @@
 import Table from "./components/table";
 
 export default function App() {
-  return <Table />;
+  return (
+    <Table
+      col1={["item1", "item2", "item3"]}
+      col2={[1, 2, 3]}
+      col1Header={"Name"}
+      col2Header={"Age"}
+    />
+  );
 }
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,3 +1,21 @@
-export default function table() {
-  return <h2>Table</h2>;
+export default function table({ col1, col2, col1Header, col2Header }) {
+  return (
+    <table className="Container ex-auto max-w-fit text-left">
+      <thead>
+        <tr>
+          <th>{col1Header}</th>
+          <th>{col2Header}</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {col1.map((item, index) => (
+          <tr key={index}>
+            <td>{item}</td>
+            <td>{col2[index]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
